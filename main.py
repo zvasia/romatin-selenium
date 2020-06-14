ced000c50b1b14b25d55307a6be276404dba554f
vasiliy <zvasia@gmail.com>
2020-06-14 21:41:08 +0300

    save to file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ options = webdriver.ChromeOptions()
 
 
 options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+# options.add_argument("--disable-gpu")
 options.add_argument('blink-settings=imagesEnabled=false')
 
 
@@ -26,11 +26,11 @@ def go_to_next_page():
     driver.find_element_by_css_selector("a[oldtitle='next page']").click()
 
 
-def get_irns_on_page():
+def get_irns_on_page(list):
     wait_for_load("td[aria-describedby='gridForsearch_pane_IRN']")
     irn_on_page_list = driver.find_elements_by_css_selector("td[aria-describedby='gridForsearch_pane_IRN']")
     for irn in irn_on_page_list:
-        irn_list.append(irn.text)
+        list.append(irn.text)
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
@@ -40,7 +40,7 @@ SEARCH_URL = 'https://www3.wipo.int/madrid/monitor/en'
 PAGE_SIZE = 100
 
 date_from = '2019-03-01'
-date_to = '2019-03-02'
+date_to = '2019-03-05'
 
 driver.get(SEARCH_URL)
 driver.find_element_by_css_selector("a#advancedModeLink").click()
@@ -54,10 +54,12 @@ page_counter = driver.find_element_by_css_selector("div.pageCount").text.split('
 total_pages = int(page_counter[1])
 irn_list = []
 for page in range(total_pages - 1):
-    get_irns_on_page()
+    get_irns_on_page(irn_list)
     go_to_next_page()
-get_irns_on_page()
-
-print(irn_list)
-print(len(irn_list))
+get_irns_on_page(irn_list)
 driver.quit()
+
+f = open("irns.txt", "w+")
+for num in irn_list:
+    f.write(num + "\n")
+f.close()
