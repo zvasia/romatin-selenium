75d74b12426945ee59a0aca3c558971be1178737
vasiliy <zvasia@gmail.com>
2020-06-17 15:17:00 +0300

    compare parsed info with database and save to diff.txt

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import argparse
+import mysql.connector
+from datetime import datetime
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,12 +11,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-df', default='*') # date_from
+    parser.add_argument('-dt', default='*') # date_to
+
+    return parser
+
+
+arguments_parser = create_argument_parser()
+namespace = arguments_parser.parse_args(sys.argv[1:])
+
+date_from = namespace.df
+date_to = namespace.dt
+
+# parse data
 options = webdriver.ChromeOptions()
-
-
 options.add_argument("--headless")
-# options.add_argument("--disable-gpu")
+options.add_argument("--disable-gpu")
 options.add_argument('blink-settings=imagesEnabled=false')
+start_time = datetime.now()
 
 
 def wait_for_load(selector):
@@ -39,9 +59,6 @@ SEARCH_URL = 'https://www3.wipo.int/madrid/monitor/en'
 
 PAGE_SIZE = 100
 
-date_from = '2019-03-01'
-date_to = '2019-03-05'
-
 driver.get(SEARCH_URL)
 driver.find_element_by_css_selector("a#advancedModeLink").click()
 driver.find_element_by_css_selector("input#RD_input").send_keys(date_from + " TO " + date_to + Keys.ENTER)
@@ -63,3 +80,51 @@ f = open("irns.txt", "w+")
 for num in irn_list:
     f.write(num + "\n")
 f.close()
+
+
+# compare with current database
+config = {
+    'user': 'root',
+    'password': 'root',
+    'port': '8889',
+    'host': '127.0.0.1',
+    'database': 'romarin',
+    'raise_on_warnings': True,
+}
+
+
+def get_site_nums():
+    f = open("irns.txt", "r")
+    parsed_irns = f.read().split('\n')
+    parsed_irns = filter(None, parsed_irns)
+    f.close()
+    return parsed_irns
+
+
+def get_database_nums(start_date, end_date):
+    link = mysql.connector.connect(**config)
+    cursor = link.cursor()
+    query = "SELECT INTREGN FROM TOSN WHERE (INTREGD BETWEEN '{start_date}' AND '{end_date}')"\
+        .format(start_date=start_date, end_date=end_date)
+    cursor.execute(query)
+    tms_in_db = cursor.fetchall()
+    trademarks_in_db = []
+    for x in tms_in_db:
+        trademarks_in_db.append(x[0])
+    cursor.close()
+    link.close()
+    return trademarks_in_db
+
+
+site_nums = get_site_nums()
+db_nums = get_database_nums(date_from, date_to)
+site_nums_set = set(site_nums)
+db_nums_set = set(db_nums)
+diff = site_nums_set - db_nums_set
+
+
+f = open("diff.txt", "w+")
+for num in diff:
+    f.write(num + "\n")
+f.close()
+
